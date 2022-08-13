9e5e06519a767cdaba21b88e60e05aa9ae3040b7
Frederico Hakamine <frederico.hakamine@gmail.com>
2022-08-13 09:35:47 -0700

    moved env to support static builds

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const withMarkdoc = require('@markdoc/next.js')
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
+  env: {
+    NEXT_PUBLIC_DOCSEARCH_APP_ID: "RYVBZO8R7U",
+    NEXT_PUBLIC_DOCSEARCH_API_KEY: "af9265c82b63d7e721c1884a1bd12341",
+    NEXT_PUBLIC_DOCSEARCH_INDEX_NAME: "webhooks"
+  },
 }
 
 module.exports = withMarkdoc()(nextConfig)
